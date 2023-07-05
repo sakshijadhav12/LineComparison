d403d074f642a0e5fcf89e576b53d70e2c56ea51
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-06 00:40:42 +0530

    [sakshi] welcome to Line Comparison Computation Program

---
+++ LineComparison/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to class Camparison Computation Program");
+        }
+    }
+}
